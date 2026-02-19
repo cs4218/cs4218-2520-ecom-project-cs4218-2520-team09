cf14d88c452ef5799de610e36eba28892233fc29
JOHN\Flare <derpicon12@gmail.com>
2026-02-19 16:23:25 +0800

    no message

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -1,36 +1,39 @@
 export default {
-  // name displayed during tests
-  displayName: "frontend",
-
-  // simulates browser environment in jest
-  // e.g., using document.querySelector in your tests
-  testEnvironment: "jest-environment-jsdom",
-
-  // jest does not recognise jsx files by default, so we use babel to transform any jsx files
-  transform: {
-    "^.+\\.jsx?$": "babel-jest",
-  },
-
-  // tells jest how to handle css/scss imports in your tests
-  moduleNameMapper: {
-    "\\.(css|scss)$": "identity-obj-proxy",
-  },
-
-  // ignore all node_modules except styleMock (needed for css imports)
-  transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
-
-  // only run these tests
-  testMatch: ["<rootDir>/client/src/pages/**/*.test.js"],
-
-  // jest code coverage
-  collectCoverage: true,
-//   collectCoverageFrom: ["client/src/pages/**/**"],
-  collectCoverageFrom: ["client/src/pages/CartPage.js"],
-  coverageThreshold: {
-    global: {
-      lines: 100,
-      functions: 100,
+    // name displayed during tests
+    displayName: "frontend",
+  
+    // simulates browser environment in jest
+    // e.g., using document.querySelector in your tests
+    testEnvironment: "jest-environment-jsdom",
+  
+    // jest does not recognise jsx files by default, so we use babel to transform any jsx files
+    transform: {
+      "^.+\\.jsx?$": "babel-jest",
     },
-  },
-  setupFilesAfterEnv: ["<rootDir>/client/src/setupTests.js"],
-};
+  
+    // tells jest how to handle css/scss imports in your tests
+    moduleNameMapper: {
+      "\\.(css|scss)$": "identity-obj-proxy",
+    },
+  
+    // ignore all node_modules except styleMock (needed for css imports)
+    transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
+  
+    // only run these tests
+    testMatch: ["<rootDir>/client/src/**/*.test.js"],
+  
+    // jest code coverage
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "client/src/pages/Auth/Login.js",
+        "client/src/pages/Auth/Register.js",
+        "client/src/pages/admin/AdminDashboard.js"
+    ],
+  //   coverageThreshold: {
+  //     global: {
+  //       lines: 100,
+  //       functions: 100,
+  //     },
+  //   },
+    setupFilesAfterEnv: ["<rootDir>/client/src/setupTests.js"],
+  };
