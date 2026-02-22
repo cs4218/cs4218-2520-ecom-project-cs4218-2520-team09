349010442fec1dc6efd055182df3ae35366bd00b
Yiwei <123lyw321@gmail.com>
2026-02-23 00:21:26 +0800

    Policy fix

--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -4,17 +4,20 @@ import Layout from "./../components/Layout";
 const Policy = () => {
   return (
     <Layout title={"Privacy Policy"}>
-      <div className="row contactus ">
+      {/* Liu, Yiwei, A0332922J */}
+      <div className="row policy-container">
         <div className="col-md-6 ">
+          {/* Liu, Yiwei, A0332922J */}
           <img
-            src="/images/contactus.jpeg"
-            alt="contactus"
+            src="/images/policy.jpeg"
+            alt="privacy policy"
             style={{ width: "100%" }}
           />
         </div>
         <div className="col-md-4">
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
+          {/* Liu, Yiwei, A0332922J */}
+          <h1>Privacy Policy</h1>
+          <p>We value your privacy and are committed to protecting your personal data.</p>
           <p>add privacy policy</p>
           <p>add privacy policy</p>
           <p>add privacy policy</p>
